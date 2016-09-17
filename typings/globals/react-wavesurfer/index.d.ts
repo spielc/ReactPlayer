8d0c155851db0da640f25cb90278960c73483bf3
Christoph Spielmann <spielc@gmail.com>
2016-09-17 13:06:03 +0200

    Simplify module declaration for react-wavesurfer

--- typings/globals/react-wavesurfer/index.d.ts
+++ typings/globals/react-wavesurfer/index.d.ts
@@ -1,53 +1,51 @@
-declare namespace __React {
-    //namespace WaveSurfer {
-
-        interface WaveSurferProperties {
-            scrollParent: boolean;
-            height: number;
-            progressColor: string;
-            waveColor: string;
-            normalize: boolean;
-            barWidth: number;
-        }
-
-        interface OnPosChangeParam {
-            wavesurfer: WaveSurfer;
-            originalArgs: number[];
-        }
-
-        interface OnFinishParam {
-            wavesurfer: WaveSurfer;
-        }
-
-        interface WaveSurferComponentProperties {
-            playing: boolean;
-            volume: number;
-            //zoom: number;
-            pos: number;
-            //audioPeaks:
-            audioFile: string | Blob | File;
-            options?: WaveSurferProperties;
-
-            onReady?: () => void;
-            onPosChange?: (param: OnPosChangeParam) => void;
-            onFinish?: (wavesurfer: OnFinishParam) => void;
-            //mediaElt: string | HTMLElement; 
-        }
-
-        /**
-         * WaveSurfer
-         */
-        class WaveSurfer extends Component<WaveSurferComponentProperties,{}> {
-            constructor(props: WaveSurferComponentProperties, context?: any);
-            render(): JSX.Element;
-            componentDidMount(): void;
-            componentWillReceiveProps(nextProps: WaveSurferComponentProperties, nextContext: any): void;
-            componentWillUnmount(): void;
-        }
-    //}
-}
-
 declare module "react-wavesurfer" {
-    import WaveSurfer = __React.WaveSurfer;
+
+    import React = require("react");
+ 
+    interface WaveSurferProperties {
+        scrollParent: boolean;
+        height: number;
+        progressColor: string;
+        waveColor: string;
+        normalize: boolean;
+        barWidth: number;
+    }
+
+    interface OnPosChangeParam {
+        wavesurfer: WaveSurfer;
+        originalArgs: number[];
+    }
+
+    interface OnFinishParam {
+        wavesurfer: WaveSurfer;
+    }
+
+    interface WaveSurferComponentProperties {
+        playing: boolean;
+        volume: number;
+        //zoom: number;
+        pos: number;
+        //audioPeaks:
+        audioFile: string | Blob | File;
+        options?: WaveSurferProperties;
+
+        onReady?: () => void;
+        onPosChange?: (param: OnPosChangeParam) => void;
+        onFinish?: (wavesurfer: OnFinishParam) => void;
+        //mediaElt: string | HTMLElement; 
+    }
+
+    /**
+     * WaveSurfer
+     */
+    class WaveSurfer extends React.Component<WaveSurferComponentProperties,{}> {
+        constructor(props: WaveSurferComponentProperties, context?: any);
+        render(): JSX.Element;
+        componentDidMount(): void;
+        componentWillReceiveProps(nextProps: WaveSurferComponentProperties, nextContext: any): void;
+        componentWillUnmount(): void;
+    }
+
     export default WaveSurfer;
+
 }
