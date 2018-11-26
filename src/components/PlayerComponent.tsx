6dfed76bd10c37271d94b3c4812be4b33c110520
Christoph Spielmann <spielc@gmail.com>
2018-11-26 20:30:55 +0100

    Update wavesurfer.js and remove some unnecessary dependencies

--- src/components/PlayerComponent.tsx
+++ src/components/PlayerComponent.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {autorun} from "mobx";
 import {observer} from "mobx-react";
-import WaveSurfer from "react-wavesurfer";
-import {readFile} from "fs";
-import {request, RequestOptions} from "http";
-import md5 from "md5";
+import * as WaveSurfer from "wavesurfer.js";
 const {BrowserWindow} = require("electron").remote;
 
-import {ComponentWithSettings, ComponentWithSettingsProperties} from "./ComponentWithSettings";
 import {PlayerState, DocumentType} from "../base/enums";
 import {AppState} from "../base/appstate";
-import {Track} from "../base/track";
-import {Playlist} from "../base/playlist";
-import { PlayerMessageTypes_Play, PlayerMessageTypes_Forward, PlayerMessageTypes_Backward, PlayerMessageTypes, ReactPlayerDB, CurrentSongIndexSetting, PlaybackStateSetting, LibraryModeEnabledSetting, PlaylistMessageType, PlaylistMessageTypes, PlaylistMessage_Changed, PlaylistMessage_TrackChanged, WindowManagementMessage_Define, WindowManagementMessage_Show, SettingsWindowName, WindowManagementMessage_RegisterHandler, WindowManagementMessage_LifeCycleEvent, SettingIdPrefix, PlaylistIdPrefix } from "../base/typedefs";
+import { CurrentSongIndexSetting, SettingIdPrefix } from "../base/typedefs";
 import {Setting} from "../base/setting";
 import {LastFMHelper} from "../util/LastFMHelper";
 import {mod} from "../base/util";
@@ -22,10 +14,6 @@ interface PlayerComponentProperties {
     state: AppState
 }
 
-interface WaveSurferEventParams {
-        wavesurfer: any;
-        originalArgs: any[];
-}
 
 const EnableScrobblingSetting = SettingIdPrefix + "PlayerComponent.EnableScrobbling";
 const LastFMSessionKeySetting = SettingIdPrefix + "PlayerComponent.EnableLastSessionKey";
@@ -40,59 +28,76 @@ export class PlayerComponent extends React.Component<PlayerComponentProperties,{
 
     constructor(props: PlayerComponentProperties, context?: any) {
         super(props, context);
-        this.waveSurfer=[];
+        this.waveSurfer = [];
         this.trackDurationHalf = -1;
         this.trackStartPlaybackTimestamp = -1;
         this.lastFMHelper = null;
+        
+    }
+    
+    public componentDidMount(): void {
+        for (let i=0;i<3;i++) {
+            const props = {
+                container: `#wavesurfer${i}`,
+                height: 30
+            };
+            this.waveSurfer.push(new WaveSurfer(props));
+            this.waveSurfer[i].init();
+            this.waveSurfer[i].on("audioprocess", (pos) => this.posChange(pos, this.waveSurfer[i]));
+            this.waveSurfer[i].on("ready", () => this.props.state.ready(i));
+            this.waveSurfer[i].on("finish", () => this.props.state.forward());     
+        }
     }    
 
     public render(): JSX.Element {
 
-        let style: React.CSSProperties = {
-            color: (this.props.state.libraryModeEnabled) ? "lightblue" : ""
+
+        const waveStyles: React.CSSProperties[] = [{ opacity: 0 }, { opacity: 0 }, { opacity: 0 }];
+        if (this.props.state.normalizedCurrentIndex >= 0) {
+            waveStyles[this.props.state.normalizedCurrentIndex].opacity = 1.0;
+        }
+        if (this.waveSurfer.length > 0 ) {
+            
+            for (let i=0;i<this.waveSurfer.length;i++) {
+                this.waveSurfer[i].setVolume(this.props.state.currentVolume);
+                this.waveSurfer[i].pause();
+                switch(this.props.state.state[i]) {
+                case PlayerState.Loaded:
+                    this.waveSurfer[i].load((this.props.state.currentFile[i].length === 0) ? "" : this.props.state.currentFile[i]);             
+                    break;
+                case PlayerState.Playing:
+                    this.waveSurfer[i].play();
+                    break;
+                default:
+                    break;
+                }
+            }
         }
 
-        
-        
         return (
             <div>
-                <div hidden={(this.props.state.normalizedCurrentIndex) != 0}>
-                    <WaveSurfer audioFile={(this.props.state.currentFile[0].length == 0) ? new Blob() : this.props.state.currentFile[0]} playing={this.props.state.state[0]==PlayerState.Playing} pos={0} volume={this.props.state.currentVolume} onFinish={(evt)=>this.props.state.forward()} onPosChange={(evt)=>this.posChange(evt)} onReady={() => this.props.state.ready(0)} ref={(r) => { this.waveSurfer[0]=r } } options={ {height: 30} } />
-                </div>
-                <div hidden={(this.props.state.normalizedCurrentIndex) != 1}>
-                    <WaveSurfer audioFile={(this.props.state.currentFile[1].length == 0) ? new Blob() : this.props.state.currentFile[1]} playing={this.props.state.state[1]==PlayerState.Playing} pos={0} volume={this.props.state.currentVolume} onFinish={(evt)=>this.props.state.forward()} onPosChange={(evt)=>this.posChange(evt)} onReady={() => this.props.state.ready(1)} ref={(r) => { this.waveSurfer[1]=r } } options={ {height: 30} } />
-                </div>
-                <div hidden={(this.props.state.normalizedCurrentIndex) != 2}>
-                    <WaveSurfer audioFile={(this.props.state.currentFile[2].length == 0) ? new Blob() : this.props.state.currentFile[2]} playing={this.props.state.state[2]==PlayerState.Playing} pos={0} volume={this.props.state.currentVolume} onFinish={(evt)=>this.props.state.forward()} onPosChange={(evt)=>this.posChange(evt)} onReady={() => this.props.state.ready(2)} ref={(r) => { this.waveSurfer[2]=r } } options={ {height: 30} } />
-                </div>
+                <div id="wavesurfer0" style={waveStyles[0]} />
+                <div id="wavesurfer1" style={waveStyles[1]} />
+                <div id="wavesurfer2" style={waveStyles[2]} />
                 <div id="container" className={this.props.state.containerState}>
                     <div className="player-control">
-                        <div id="previous-button" title="Previous" onClick={evt=>this.props.state.backward()} className={(this.trackChangeBtnClassName(false))}><i className="fa fa-fast-backward"/></div>
-                        <div id="play-button" title="Play" onClick={evt => this.props.state.play()}><i className="fa fa-play"/></div>
-                        <div id="pause-button" title="Pause" onClick={evt => this.props.state.pause()}><i className="fa fa-pause"/></div>
-                        <div id="stop-button" title="Stop" onClick={evt=>this.props.state.stop()}><i className="fa fa-stop"/></div>
-                        <div id="next-button" title="Next" onClick={evt=>this.props.state.forward()} className={(this.trackChangeBtnClassName(true))}><i className="fa fa-fast-forward"/></div>
-                        <div id="mute-button" title="Toggle mute" onClick={evt=>this.props.state.toggleMute()}><i className="fa fa-volume-off"></i></div>
-                        <div id="volume-down-button" title="Volume Down" onClick={evt => this.props.state.decreaseVolume()}><i className="fa fa-volume-down"/></div>
-                        <div id="volume-up-button" title="Volume Up" onClick={evt => this.props.state.increaseVolume()}><i className="fa fa-volume-up"/></div>
-                        <div id="library-button" title="Library"><i className="fa fa-book" style={{ color: (this.props.state.libraryModeEnabled) ? "lightblue" : ""}} onClick={evt => this.props.state.toggleLibraryMode()} /></div>
-                        <div id="follow-button" title="Follow"><strong style={{ color: (this.props.state.followModeEnabled) ? "lightblue" : ""}} onClick={evt => this.props.state.toggleFollowMode()}>F</strong></div>
-                        <div id="settings-button" title="Settings" onClick={evt=>this.openSettingsDialog()}><i className="fa fa-cog"/></div>
+                        <div id="previous-button" title="Previous" onClick={()=>this.props.state.backward()} className={(this.trackChangeBtnClassName(false))}><i className="fa fa-fast-backward"/></div>
+                        <div id="play-button" title="Play" onClick={() => this.props.state.play()}><i className="fa fa-play"/></div>
+                        <div id="pause-button" title="Pause" onClick={() => this.props.state.pause()}><i className="fa fa-pause"/></div>
+                        <div id="stop-button" title="Stop" onClick={()=>this.props.state.stop()}><i className="fa fa-stop"/></div>
+                        <div id="next-button" title="Next" onClick={()=>this.props.state.forward()} className={(this.trackChangeBtnClassName(true))}><i className="fa fa-fast-forward"/></div>
+                        <div id="mute-button" title="Toggle mute" onClick={()=>this.props.state.toggleMute()}><i className="fa fa-volume-off"></i></div>
+                        <div id="volume-down-button" title="Volume Down" onClick={() => this.props.state.decreaseVolume()}><i className="fa fa-volume-down"/></div>
+                        <div id="volume-up-button" title="Volume Up" onClick={() => this.props.state.increaseVolume()}><i className="fa fa-volume-up"/></div>
+                        <div id="library-button" title="Library"><i className="fa fa-book" style={{ color: (this.props.state.libraryModeEnabled) ? "lightblue" : ""}} onClick={() => this.props.state.toggleLibraryMode()} /></div>
+                        <div id="follow-button" title="Follow"><strong style={{ color: (this.props.state.followModeEnabled) ? "lightblue" : ""}} onClick={() => this.props.state.toggleFollowMode()}>F</strong></div>
+                        <div id="settings-button" title="Settings" onClick={()=>this.openSettingsDialog()}><i className="fa fa-cog"/></div>
                     </div>
                 </div>
             </div>
         );
     }
 
-    private toggleLibraryMode(event: React.MouseEvent<HTMLElement>): void {
-        let color = event.currentTarget.style.color;
-        if (color == "")
-            color = "lightblue";
-        else
-            color = "";
-        event.currentTarget.style.color = color;
-        // TODO fix that
-    }
 
     private openSettingsDialog(): void {
         let settingsWindow = new BrowserWindow({
@@ -107,11 +112,12 @@ export class PlayerComponent extends React.Component<PlayerComponentProperties,{
         return (this.props.state.currentFile[mod((this.props.state.currentIndex + changeValue), 3)] != null && this.props.state.currentFile[mod((this.props.state.currentIndex + changeValue), 3)]) ? "enabled" : "disabled";
     }
 
-    private posChange(event: WaveSurferEventParams): void {
-        var pos = Math.floor(event.originalArgs[0]);
+    private posChange(evtPos: number, waveSurfer: WaveSurfer): void {
+        var pos = Math.floor(evtPos);
         if (pos == 0) {
-            event.wavesurfer.drawBuffer();
-            let trackDuration = Math.ceil(event.wavesurfer.getDuration());
+            // event.wavesurfer.drawBuffer();
+            let trackDuration = Math.ceil(waveSurfer.getDuration());
+            // waveSurfer.Drawer.progress(pos/trackDuration);
             if (trackDuration > 30 && this.trackDurationHalf < 0) {
                 let now = new Date(); 
                 this.trackStartPlaybackTimestamp = Math.floor(now.valueOf() / 1000);
@@ -121,7 +127,7 @@ export class PlayerComponent extends React.Component<PlayerComponentProperties,{
         }
         else if (this.trackDurationHalf > 0 && ((pos > this.trackDurationHalf) || (pos > 240)))  {
             if (this.props.state.settings.length > 0) {
-                let enableScrobblingSetting = this.props.state.settings.find((setting, index, obj) => { return setting._id == EnableScrobblingSetting }) as Setting<boolean>;
+                let enableScrobblingSetting = this.props.state.settings.find((setting) => { return setting._id == EnableScrobblingSetting }) as Setting<boolean>;
                 if (enableScrobblingSetting) {
                     if (enableScrobblingSetting.Value) {
                         if (!this.lastFMHelper) {
@@ -160,27 +166,6 @@ export class PlayerComponent extends React.Component<PlayerComponentProperties,{
                     this.props.state.settings.push(lastFMSessionKeySetting);
                 }
             }
-            // var enableScrobblingSetting = this.settings.find((setting, index, obj) => { return setting._id == EnableScrobblingSetting }) as Setting<boolean>;
-            // if (enableScrobblingSetting && enableScrobblingSetting.Value) { 
-            //     this.props.db.get(`${PlaylistIdPrefix}All`).then((response) => {
-            //         let list = response as Playlist;
-            //         if (list != null) {
-            //             this.props.db.get(list.Tracks[this.state.currentIndex]._id).then(value => {
-            //                 let track = value as Track;
-            //                 let map = new Map<string,string>();
-            //                 map.set("album", track.album);
-            //                 map.set("artist", track.artist);
-            //                 map.set("method", "track.scrobble");
-            //                 map.set("timestamp", this.trackStartPlaybackTimestamp.toString());
-            //                 map.set("track", track.title);
-            //                 if (this.lastFMHelper) {
-            //                     this.lastFMHelper.startRequest(map, true);
-            //                 }
-            //             });
-            //         }
-            //     });
-            // }
-            // this.trackDurationHalf = -1;
         }
 
     }
--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,57 +1,57 @@
-import React = require("react");
-import ReactDOM = require("react-dom");
-import WaveSurfer from "react-wavesurfer";
+// import React = require("react");
+// import ReactDOM = require("react-dom");
+// import WaveSurfer from "react-wavesurfer";
 
-interface TestComponentState {
-    file: Blob
-}
+// interface TestComponentState {
+//     file: Blob
+// }
 
-export class TestComponent extends React.Component<{}, TestComponentState> {
+// export class TestComponent extends React.Component<{}, TestComponentState> {
 
-    constructor(props?: any, context?: any) {
-        super(props, context)
-        this.state = {
-            file: new Blob()
-        }
-    }
+//     constructor(props?: any, context?: any) {
+//         super(props, context)
+//         this.state = {
+//             file: new Blob()
+//         }
+//     }
 
-    private filesSelected(evt: React.FormEvent<HTMLInputElement>): void {
-        var rawObject = evt.currentTarget as any;
-        var files = rawObject.files as FileList;
-        var file = files[0];
-        var reader = new FileReader();
-        reader.onload = (event) => {
-            var fileReader=event.target as FileReader;
-            var data = fileReader.result as string;
-            var splittedData = data.split(",");
-            var base64EncodedData = splittedData[1];
-            var decodedData = atob(base64EncodedData);
-            var mimeString = data.split(',')[0].split(':')[1].split(';')[0];
-            var ab = new ArrayBuffer(decodedData.length);
-            var ia = new Uint8Array(ab);
-            for (var i = 0; i < decodedData.length; i++) {
-                ia[i] = decodedData.charCodeAt(i);
-            }
-            this.setState({
-                file: new Blob([ab])//, { type: mimeString })
-            });
-        };
-        reader.readAsDataURL(file);
-        /*this.setState({
-            file: file
-        });*/
-    }
+//     private filesSelected(evt: React.FormEvent<HTMLInputElement>): void {
+//         var rawObject = evt.currentTarget as any;
+//         var files = rawObject.files as FileList;
+//         var file = files[0];
+//         var reader = new FileReader();
+//         reader.onload = (event) => {
+//             var fileReader=event.target as FileReader;
+//             var data = fileReader.result as string;
+//             var splittedData = data.split(",");
+//             var base64EncodedData = splittedData[1];
+//             var decodedData = atob(base64EncodedData);
+//             var mimeString = data.split(',')[0].split(':')[1].split(';')[0];
+//             var ab = new ArrayBuffer(decodedData.length);
+//             var ia = new Uint8Array(ab);
+//             for (var i = 0; i < decodedData.length; i++) {
+//                 ia[i] = decodedData.charCodeAt(i);
+//             }
+//             this.setState({
+//                 file: new Blob([ab])//, { type: mimeString })
+//             });
+//         };
+//         reader.readAsDataURL(file);
+//         /*this.setState({
+//             file: file
+//         });*/
+//     }
 
-    private onReady(): void {
+//     private onReady(): void {
 
-    }
+//     }
 
-    public render(): JSX.Element {
-        return (
-            <div>
-                <input type="file" multiple={false} onChange={(evt) => this.filesSelected(evt)} />
-                <WaveSurfer audioFile = {this.state.file} playing={false} pos={0} volume={0.5} onReady={()=>this.onReady()} />
-            </div>
-        );
-    }
-}
+//     public render(): JSX.Element {
+//         return (
+//             <div>
+//                 <input type="file" multiple={false} onChange={(evt) => this.filesSelected(evt)} />
+//                 <WaveSurfer audioFile = {this.state.file} playing={false} pos={0} volume={0.5} onReady={()=>this.onReady()} />
+//             </div>
+//         );
+//     }
+// }
